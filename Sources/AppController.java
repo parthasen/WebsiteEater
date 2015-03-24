a621aa1981e5b9ea4e927c35135ab297e8014ac4
jeremyvizzini <contact@jeremyvizzini.com>
2015-03-25 00:50:56 +0100

    Replace tabs to soft tabs (4 spaces)

--- Sources/AppController.java
+++ Sources/AppController.java
@@ -9,35 +9,35 @@ import java.awt.*;
 import javax.swing.*;
 
 public class AppController {
-	
-	// Properties
-	
-	private MainFrameController mainFrameController;
-		
-	// Constructors
-	
-	private static class AppControllerHolder {
-		static AppController instance = new AppController();    
-	}
+    
+    // Properties
+    
+    private MainFrameController mainFrameController;
+        
+    // Constructors
+    
+    private static class AppControllerHolder {
+        static AppController instance = new AppController();    
+    }
 
-	public static AppController getSharedController() {
-	  return AppControllerHolder.instance;
-	}
-	
-	private AppController() {
-		this.mainFrameController = new MainFrameController();
-	}
-	
-	// Getters / Setters
-	
-	public MainFrameController getMainFrameController() {
-		return this.mainFrameController;
-	}
-  	
-	// Manage frames
-	
-	public void showMainFrame() {
-		this.mainFrameController.showFrame();
-	}
-	
+    public static AppController getSharedController() {
+      return AppControllerHolder.instance;
+    }
+    
+    private AppController() {
+        this.mainFrameController = new MainFrameController();
+    }
+    
+    // Getters / Setters
+    
+    public MainFrameController getMainFrameController() {
+        return this.mainFrameController;
+    }
+    
+    // Manage frames
+    
+    public void showMainFrame() {
+        this.mainFrameController.showFrame();
+    }
+    
 }
--- Sources/WebsiteEater.java
+++ Sources/WebsiteEater.java
@@ -9,16 +9,16 @@ import java.awt.*;
 import javax.swing.*;
 
 public class WebsiteEater {
-	
-	public static void main(String[] args) {
-		if (args.length == 0) {
-			AppController.getSharedController().showMainFrame();
-		} else {
-			executeCommandLine(args);
-		}
-	}    
-	
-	public static void executeCommandLine(String[] args) {
-		System.out.println("Command line interface is not yet supported");
-	}
+    
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            AppController.getSharedController().showMainFrame();
+        } else {
+            executeCommandLine(args);
+        }
+    }    
+    
+    public static void executeCommandLine(String[] args) {
+        System.out.println("Command line interface is not yet supported");
+    }
 }
