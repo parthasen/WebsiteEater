0ba84b48fc418027d22dfbbf2461459d407090f5
jeremyvizzini <contact@jeremyvizzini.com>
2015-03-26 15:54:04 +0100

    Begin of layout implementation

--- Sources/MainFrameController.java
+++ Sources/MainFrameController.java
@@ -21,16 +21,50 @@ public class MainFrameController {
     // Contructors
     
     public MainFrameController() {
+        this.setupFrame();
+		this.setupLayout();
+    }
+	
+	private void setupFrame() {
         this.frame.setTitle("Website Eater");
         this.frame.setSize(600, 400);
         this.frame.setLocationRelativeTo(null);
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
-        this.frame.setResizable(false); 
+        this.frame.setResizable(true); 
         
         this.panel.setBackground(Color.WHITE);  
         this.panel.add(this.button);
         this.frame.setContentPane(this.panel); 
-    }
+	}
+	
+	private void setupLayout() {
+
+		this.frame.setLayout(new BorderLayout());
+		    //On ajoute le bouton au content pane de la JFrame
+		    //Au centre
+		    this.panel.add(new JButton("CENTER"), BorderLayout.CENTER);
+		    //Au nord
+		    this.panel.add(new JButton("NORTH"), BorderLayout.NORTH);
+		    //Au sud
+		    this.panel.add(new JButton("SOUTH"), BorderLayout.SOUTH);
+		    //À l'ouest
+		    this.panel.add(new JButton("WEST"), BorderLayout.WEST);
+		    //À l'est
+			JButton button = new JButton("EAST");
+			Dimension dim = new Dimension(200, 200);
+			button.setPreferredSize(dim);
+		    this.panel.add(button, BorderLayout.EAST);
+			
+		/*
+	    this.frame.setLayout(new GridLayout(3, 2));
+		
+
+	    this.panel.add(new JButton("1"));
+	    this.panel.add(new JButton("2"));
+	    this.panel.add(new JButton("3"));
+	    this.panel.add(new JButton("4"));
+	    this.panel().add(new JButton("5"));*/
+	}
     
     // Getters / Setters
     
