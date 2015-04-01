347cd0f1b6ea2855bdff68c5fc90710d87633ead
Louis Sellam <louis.sellam@gmail.com>
2015-04-01 23:10:11 +0200

    First layout disposition done

--- Sources/MainFrameController.java
+++ Sources/MainFrameController.java
@@ -18,48 +18,97 @@ public class MainFrameController {
 	JPanel panelNorth = new JPanel();
 	JPanel panelCenter = new JPanel();	
 
-	// Contructors
+	//Panel for the panelCenter
+	JPanel panelCenterCenter = new JPanel();
 
+	//Panel for the north panel
+	JPanel panelNorthCenter = new JPanel();	
+
+	JPanel panelNorthCenterCenter = new JPanel();		//panel for both text field
+	JPanel panelNorthCenterCenterCenter = new JPanel();	//panel for directory text field
+	JPanel panelNorthCenterCenterNorth = new JPanel();	//panel for url text field
+
+	JPanel panelNorthCenterEast = new JPanel();			//panel for both buttons
+	JPanel panelNorthCenterEastCenter = new JPanel();	//panel for directory button
+	JPanel panelNorthCenterEastNorth = new JPanel();	//panel for launch button
+
+	//Button
+	JButton launch = new JButton("Launch");
+	JButton directoryChooserButton = new JButton("Dir");
+
+	// Contructors
 	public MainFrameController() {
-		this.mainSetupFrame();
-		this.mainSetupLayout();
-		//this.panelNorthSetupLayout();
+		this.setupMainFrame();
+		this.setupMainLayout();
 	}
 
-	private void mainSetupFrame() {
+	private void setupMainFrame() {
 		this.frame.setTitle("Website Eater");
-		this.frame.setSize(600, 400);
+		this.frame.setSize(600, 500);
 		this.frame.setLocationRelativeTo(null);
 		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
-		this.frame.setResizable(false);         
-
-		//Dimension dimMaximumWindowSize = new Dimension(800, 800);
-		//this.frame.setMaximumSize(dimMaximumWindowSize);
+		this.frame.setResizable(false); 		
 		this.frame.setBackground(Color.WHITE);  
 		this.frame.setContentPane(this.panel); 
+		//Dimension dimMaximumWindowSize = new Dimension(800, 800);
+		//this.frame.setMaximumSize(dimMaximumWindowSize);
 	}
 
-	private void mainSetupLayout() {
+	private void setupMainLayout() {
 		this.frame.setLayout(new BorderLayout());
-		Dimension dimPanelNorth = new Dimension(600, 100);
+		Dimension dimPanelNorth = new Dimension(600, 120);
 		panelNorth.setPreferredSize(dimPanelNorth);
-		panelNorth.setBackground(Color.YELLOW);
-		this.panel.add(panelNorth, BorderLayout.NORTH);		
-		panelCenter.setBackground(Color.BLACK);
-		this.panel.add(panelCenter, BorderLayout.CENTER);	
+		this.panel.add(panelNorth, BorderLayout.NORTH);	
+		panelCenterCenter.setBorder(BorderFactory.createLineBorder(Color.black));
+		panelCenterCenter.setBackground(Color.WHITE);
+		this.panel.add(panelCenterCenter, BorderLayout.CENTER);	
 		this.panel.add(new JPanel(), BorderLayout.EAST);
 		this.panel.add(new JPanel(), BorderLayout.SOUTH);
-		this.panel.add(new JPanel(), BorderLayout.WEST);					
+		this.panel.add(new JPanel(), BorderLayout.WEST);	
+		this.setupPanelNorthLayout();
 	}
 
-	private void panelNorthSetupLayout() {
+	private void setupPanelNorthLayout() {
 		panelNorth.setLayout(new BorderLayout());
-		this.panel.add(new JPanel(), BorderLayout.EAST);
-		this.panel.add(new JPanel(), BorderLayout.SOUTH);
-		this.panel.add(new JPanel(), BorderLayout.WEST);
-		this.panel.add(new JPanel(), BorderLayout.CENTER);
-		this.panel.add(new JPanel(), BorderLayout.NORTH);
+		panelNorth.add(new JPanel(), BorderLayout.EAST);
+		panelNorth.add(new JPanel(), BorderLayout.NORTH);
+		panelNorth.add(new JPanel(), BorderLayout.SOUTH);
+		panelNorth.add(new JPanel(), BorderLayout.WEST);
+		//panelNorthCenter.setBackground(Color.BLUE);
+		panelNorth.add(panelNorthCenter, BorderLayout.CENTER);
+
+		panelNorthCenter.setLayout(new BorderLayout());			
+		panelNorthCenterEast.setPreferredSize(new Dimension(80, 10));
+		//panelNorthCenterEast.setBackground(Color.GREEN);
+		panelNorthCenter.add(panelNorthCenterEast, BorderLayout.EAST);
+		//panelNorthCenterCenter.setBackground(Color.BLACK);
+		panelNorthCenter.add(panelNorthCenterCenter, BorderLayout.CENTER);
+
+		panelNorthCenterEast.setLayout(new BorderLayout());
+		panelNorthCenterEastNorth.setPreferredSize(new Dimension(0, 50));
+		//panelNorthCenterEastNorth.setBackground(Color.GREEN);
+		panelNorthCenterEast.add(panelNorthCenterEastNorth, BorderLayout.NORTH);
+		launch.setPreferredSize(new Dimension(80, 40));
+		panelNorthCenterEastNorth.add(launch);		
+		panelNorthCenterEast.add(panelNorthCenterEastCenter, BorderLayout.CENTER);
+		directoryChooserButton.setPreferredSize(new Dimension(80, 40));
+		panelNorthCenterEastCenter.add(directoryChooserButton);
+
+		panelNorthCenterCenter.setLayout(new BorderLayout());
+		panelNorthCenterCenterNorth.setPreferredSize(new Dimension(0, 50));
+		//panelNorthCenterEastNorth.setBackground(Color.GREEN);
+		panelNorthCenterCenter.add(panelNorthCenterCenterNorth, BorderLayout.NORTH);
+		JTextField url = new JTextField("URL HERE");
+		url.setPreferredSize(new Dimension(490, 41));
+		panelNorthCenterCenterNorth.add(url);
+
+		panelNorthCenterCenter.add(panelNorthCenterCenterCenter, BorderLayout.CENTER);
+		JTextField directory = new JTextField("DIRECTORY");
+		directory.setPreferredSize(new Dimension(490, 41));
+		panelNorthCenterCenterCenter.add(directory);
+
 	}
+
 
 	// Getters / Setters
 
