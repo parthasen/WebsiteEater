ecf0fd6be27dc4aefd9c7cdf7e7e4ce3016a1bd8
Louis Sellam <louis.sellam@gmail.com>
2015-04-01 20:57:03 +0200

    Tabs problem solved

--- Sources/MainFrameController.java
+++ Sources/MainFrameController.java
@@ -9,36 +9,36 @@ import java.awt.*;
 import javax.swing.*;
 
 public class MainFrameController {
-    
-    // Properties    
-    JFrame frame = new JFrame();    
-    JPanel panel = new JPanel();	
-    
-    //Panel for the main Layout
+
+	// Properties    
+	JFrame frame = new JFrame();    
+	JPanel panel = new JPanel();	
+
+	//Panel for the main Layout
 	JPanel panelNorth = new JPanel();
 	JPanel panelCenter = new JPanel();	
-    
-    // Contructors
-    
-    public MainFrameController() {
-        this.mainSetupFrame();
+
+	// Contructors
+
+	public MainFrameController() {
+		this.mainSetupFrame();
 		this.mainSetupLayout();
 		//this.panelNorthSetupLayout();
-    }
-	
-	private void mainSetupFrame() {
-        this.frame.setTitle("Website Eater");
-        this.frame.setSize(600, 400);
-        this.frame.setLocationRelativeTo(null);
-        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
-        this.frame.setResizable(false);         
-        
-        //Dimension dimMaximumWindowSize = new Dimension(800, 800);
-        //this.frame.setMaximumSize(dimMaximumWindowSize);
-        this.frame.setBackground(Color.WHITE);  
-        this.frame.setContentPane(this.panel); 
 	}
-	
+
+	private void mainSetupFrame() {
+		this.frame.setTitle("Website Eater");
+		this.frame.setSize(600, 400);
+		this.frame.setLocationRelativeTo(null);
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
+		this.frame.setResizable(false);         
+
+		//Dimension dimMaximumWindowSize = new Dimension(800, 800);
+		//this.frame.setMaximumSize(dimMaximumWindowSize);
+		this.frame.setBackground(Color.WHITE);  
+		this.frame.setContentPane(this.panel); 
+	}
+
 	private void mainSetupLayout() {
 		this.frame.setLayout(new BorderLayout());
 		Dimension dimPanelNorth = new Dimension(600, 100);
@@ -51,7 +51,7 @@ public class MainFrameController {
 		this.panel.add(new JPanel(), BorderLayout.SOUTH);
 		this.panel.add(new JPanel(), BorderLayout.WEST);					
 	}
-	
+
 	private void panelNorthSetupLayout() {
 		panelNorth.setLayout(new BorderLayout());
 		this.panel.add(new JPanel(), BorderLayout.EAST);
@@ -60,21 +60,21 @@ public class MainFrameController {
 		this.panel.add(new JPanel(), BorderLayout.CENTER);
 		this.panel.add(new JPanel(), BorderLayout.NORTH);
 	}
-    
-    // Getters / Setters
-    
-    public JFrame getFrame() {
-        return this.frame;
-    }
-    
-    // Manage window state
-    
-    public void showFrame() {        
-        this.frame.setVisible(true);
-    }
-    
-    public void hideFrame() {        
-        this.frame.setVisible(false);
-    }
-    
+
+	// Getters / Setters
+
+	public JFrame getFrame() {
+		return this.frame;
+	}
+
+	// Manage window state
+
+	public void showFrame() {        
+		this.frame.setVisible(true);
+	}
+
+	public void hideFrame() {        
+		this.frame.setVisible(false);
+	}
+
 }
